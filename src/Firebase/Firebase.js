4815aa016541e133346c34a2ca0ef794870311dc
Akash16s <akashs1698@gmail.com>
2021-05-27 18:06:32 +0530

    added database url

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -6,6 +6,7 @@ const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
