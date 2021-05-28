8b8e0d2062daccb3033b1e9f99f485f55d6d2c17
chetan-cv <chetanverma99.cv@gmail.com>
2021-05-28 13:59:12 +0530

    data fetch

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,36 +1,84 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './home.css';
-import {firestore} from '../../Firebase/Firebase';
+import { firestore } from '../../Firebase/Firebase';
+import DropdownButton from 'react-bootstrap/DropdownButton';    
 
-const Home = () =>{
+const Home = () => {
 
-    const [tactics,setTactics] = useState([]);
+    const [techniques,setTechniques] = useState([]);
 
-    const fetchData = async () => {
-        var temp = [];
-        const response = firestore.collection(`tactics`);
-        const data = await response.get();
+    const temp =[
+        { 'techniqueId': 'TA0043', 'techniqueName': 'Reconnaissance', 'tactics': [], },
+        { 'techniqueId': 'TA0042', 'techniqueName': 'Resource Development', 'tactics': [] },
+        { 'techniqueId': 'TA0001', 'techniqueName': 'Initial Access', 'tactics': [] },
+        { 'techniqueId': 'TA0002', 'techniqueName': 'Execution', 'tactics': [] },
+        { 'techniqueId': 'TA0003', 'techniqueName': 'Persistence', 'tactics': [] },
+        { 'techniqueId': 'TA0004', 'techniqueName': 'Privilege Escalation', 'tactics': [] },
+        { 'techniqueId': 'TA0005', 'techniqueName': 'Defense Evasion', 'tactics': [] },
+        { 'techniqueId': 'TA0006', 'techniqueName': 'Credential Access', 'tactics': [] },
+        { 'techniqueId': 'TA0007', 'techniqueName': 'Discovery', 'tactics': [] },
+        { 'techniqueId': 'TA0008', 'techniqueName': 'Lateral Movement', 'tactics': [] },
+        { 'techniqueId': 'TA0009', 'techniqueName': 'Collection', 'tactics': [] },
+        { 'techniqueId': 'TA0011', 'techniqueName': 'Command and Control', 'tactics': [] },
+        { 'techniqueId': 'TA0010', 'techniqueName': 'Exfiltration', 'tactics': [] },
+        { 'techniqueId': 'TA0040', 'techniqueName': 'Impact', 'tactics': [] }];
 
-        data.docs.forEach((item) => {
-            // temp.push( {
-            //     id:item.id,
-            //     color: item.data().complete ? 'green': '    black',
-            //     data: item.data()
 
-            // });  
-            console.log(item);
-        })
+    const fetchTactics = () => {
 
+        console.log('fetchTacitc function called');
+
+        temp.forEach(async (technique) => {
+
+            console.log('first for each called');
+
+            let collection = `tactics/${technique.techniqueId}/techniques`;
+            const response = firestore.collection(collection);
+            const data = await response.get();
+            data.docs.forEach((item) => {
+                technique.tactics.push(
+                    {
+                        "tacticPrimeName": item.data().primeName,
+                        "tacticName": item.data().name,
+                        "tacticId": item.data().id
+                    }
+                );
+            });
+        });
+        setTechniques(temp);
     }
     useEffect(() => {
-        fetchData();
+        fetchTactics();
     }, [])
 
     return (
         <div className='home'>
-           <p>
-            Security Assesment
-            </p> 
+            <p>
+                Security Assesment
+            </p>
+            <section className='techniquesTiles'>
+                {
+                    techniques.map((item) => (
+                        <article key={item.techniqueId} className="tilesContent">
+                            <p className='heading'>
+                                {item.techniqueName}
+                            </p>
+                            <p className='subtitle'>
+                                {item.tactics.length} techniques
+                        </p>
+                            <div className='tactics'>
+                            {
+                                item.tactics.map( (item)=>(
+                                    <article key={item.tacticId}>
+                                        {item.tacticName}
+                                    </article>
+                                ))
+                            }
+                        </div>
+                        </article>
+                    ))
+                }
+            </section>
         </div>
     );
 }
